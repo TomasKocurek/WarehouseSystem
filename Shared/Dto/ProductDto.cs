661af519b35cefdde71e2d485949c461264bb0c3
Tomáš Kocůrek <49448855+TomasKocurek@users.noreply.github.com>
2023-04-01 11:03:47 +0200

    Add box info (#12)
    * added box size into db
    
    * added box info into frontend
    
    ---------
    
    Co-authored-by: Tomáš Kocůrek <tomas.kocurek@itixo.com>

--- Shared/Dto/ProductDto.cs
+++ Shared/Dto/ProductDto.cs
@@ -6,6 +6,6 @@ public class ProductDto
     public Guid Id { get; set; }
     public string Name { get; set; }
     public List<StockItemDto> StockItems { get; set; } = new();
-    public decimal SpaceRequirements { get; set; }
     public Money Price { get; set; }
+    public Size PackageSize { get; set; }
 }
